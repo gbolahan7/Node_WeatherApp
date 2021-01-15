eee4579ed946025ac821f1a13e6055458a5dcfa3
abass <abass_gbolahan@yahoo.com>
2021-01-15 09:34:00 +0100

    pulling data from weather api

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const request = require('request');
 
-const url = 'http://api.weatherstack.com/current?access_key=64de763c2d8dd189798c1968ea7b72de&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=64de763c2d8dd189798c1968ea7b72de&query=37.8267,-122.4233&units=m'
 
-request({url: url}, (error, response) => {
-const data = JSON.parse(response.body)
-console.log(data.current);
+request({url: url, json: true}, (error, response) => {
+console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. it feels like ' + response.body.current.feelslike + ' degrees out.' )
 })
